c1c1423db9e17121604dd77ec61b9c5f490fb733
Alentarii <alentarii03@gmail.com>
2025-04-01 09:21:18 +0300

    Добавлен рекурсивный обход по директории

--- TestSpecVuz.cpp
+++ TestSpecVuz.cpp
@@ -4,18 +4,50 @@
 #include <fstream>
 #include <queue>
 
+void getRecurs(std::queue<std::string>&, const std::string&);
 
 int main(int argc, char** argv) {
 
+    setlocale(LC_ALL, "rus");
+    
     if (argc == 1) { // если в аргументах только имя программы
-        std::cout << "no arguments!" << std::endl; // выводим, что нет аргументов
+        std::cout << "Вы не ввели путь" << std::endl; // выводим, что нет аргументов
     }
     else {
-        // иначе выводим все аргументы, которые переданы
+
         for (int i = 1; i < argc; i++) {
-            std::cout << "argv[" << i << "] - " << argv[i] << std::endl;
+
+            std::string put = argv[i];
+            std::queue<std::string> paths;
+
+            getRecurs(paths, put);
+
+            if (!paths.empty()) {
+                for (int i = 0; i < paths.size(); ++i) {
+                    std::cout << paths.front() << std::endl;
+                    paths.pop(); //удаляем первый
+                }
+            }
         }
     }
 
     return 0;
+}
+
+void getRecurs(std::queue<std::string>& paths, const std::string& path) {
+
+    try {
+        for (const auto& file : std::filesystem::directory_iterator(path)) { //проходим по дирректории
+            if (std::filesystem::is_directory(file)) { //если является дирректорией
+                getRecurs(paths, file.path().string());
+            }
+            else {
+                paths.push(file.path().string());
+            }
+        }
+    }
+    catch (std::filesystem::filesystem_error  const& ex) {
+        std::cout << "Исключение: " << ex.what() << std::endl;
+    }
+
 }
