4bd9d565be4cabc3f353670138109c1bb9719b4a
Alentarii <alentarii03@gmail.com>
2025-04-01 08:58:33 +0300

    Добавьте файлы проекта.

---
+++ TestSpecVuz.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <queue>
+
+
+int main(int argc, char** argv) {
+
+    if (argc == 1) { // если в аргументах только имя программы
+        std::cout << "no arguments!" << std::endl; // выводим, что нет аргументов
+    }
+    else {
+        // иначе выводим все аргументы, которые переданы
+        for (int i = 1; i < argc; i++) {
+            std::cout << "argv[" << i << "] - " << argv[i] << std::endl;
+        }
+    }
+
+    return 0;
+}
