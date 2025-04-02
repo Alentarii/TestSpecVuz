a72a44a3118b57c46c0e35cff595de9f0550baef
Alentarii <alentarii03@gmail.com>
2025-04-02 10:32:46 +0300

    Сжатие готово

--- TestSpecVuz.cpp
+++ TestSpecVuz.cpp
@@ -3,7 +3,7 @@
 #include <filesystem>
 #include <fstream>
 #include "ThreadPool.cpp"
-#include "hcl\huffmantool.h"
+#include "hcl/huffmantool.h"
 
 
 void getRecurs(std::queue<std::string>&, const std::string&);
@@ -23,6 +23,8 @@ int main(int argc, char** argv) {
 
             std::string put = argv[i]; //получаем путь
 
+            std::cout << "Ваш путь: " << put << std::endl;
+
             std::queue<std::string> paths;
 
             getRecurs(paths, put); //рекурсивно получаем файлы
@@ -33,12 +35,16 @@ int main(int argc, char** argv) {
 
                 Thread_pool t(cores_count);
 
-                for (int i = 0; i < paths.size(); ++i) {
+                while (!paths.empty()) {
+
+                    std::cout << paths.front() << std::endl;
+
                     t.add_task(Arhivator, paths.front());  //потоки пошли в бой
                     paths.pop();
                 }
 
                 t.wait_all();
+                std::cout << "Готово!";
             }
         }
 
@@ -66,5 +72,17 @@ void getRecurs(std::queue<std::string>& paths, const std::string& path) {
 }
 
 void Arhivator(std::string path) { //выполняем архивирование файлов, поточная функция
-    //сжатие
+
+    huffmantool ht;
+
+    try {
+        std::string compressedFile = path;
+        size_t start{ compressedFile.find_first_of(".") }; // Находим конец подстроки
+        compressedFile.insert(start, "_compress");    
+        std::string output = ht.compressFile(path, compressedFile);
+    }
+    catch (const char* error_message) {
+        std::cout << error_message << std::endl;
+    }
+    
 }
